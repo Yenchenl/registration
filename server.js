c431e3b621ca624fd5dffb0e2b993ae54c5971ea
Chen <73538798+Yenchenl@users.noreply.github.com>
2023-08-30 18:31:24 +0800

    add the database, but it display null

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const mysql = require('mysql2');
+const app = express();
+const port = 3000;
+const path = require('path');
+const userRoutes = require('./user'); // 導入 user.js 模組
+
+// 靜態文件中間件
+app.use(express.static(path.join(__dirname, 'public')));
+
+// 建立與資料庫的連線
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'member'
+});
+
+// 測試資料庫連線
+db.connect(err => {
+  if (err) {
+    console.error('資料庫連線失敗：', err);
+  } else {
+    console.log('資料庫連線成功');
+  }
+});
+
+// 使用 JSON 解析中間件
+app.use(express.json());
+
+// 使用 userRoutes 模組處理用戶相關路由
+app.use('/', userRoutes);
+
+app.listen(port, () => {
+  console.log(`伺服器運行在 http://localhost:${port}`);
+});
---
+++ user.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router = express.Router();
+const mysql = require('mysql2');
+
+// 建立與資料庫的連線
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'member'
+});
+
+// 測試資料庫連線
+db.connect(err => {
+  if (err) {
+    console.error('資料庫連線失敗：', err);
+  } else {
+    console.log('資料庫連線成功');
+  }
+});
+
+// 處理註冊請求
+router.post('/register', (req, res) => {
+  const { firstname, lastname, email, newpassword } = req.body;
+  console.log(req.body);
+  
+  // 插入資料到資料庫
+  const query = 'INSERT INTO users (firstname, lastname, email, newpassword) VALUES (?, ?, ?, ?)';
+  db.query(query, [firstname, lastname, email, newpassword], (err, result) => {
+    if (err) {
+      console.error('資料庫操作失敗：', err);
+      res.status(500).json({ message: '註冊失敗' });
+    } else {
+      console.log('註冊成功');
+      res.json({ message: '註冊成功' });
+    }
+  });
+});
+
+module.exports = router;
