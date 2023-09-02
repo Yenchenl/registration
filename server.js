a3e3eb64ffbc8e89285140a8dc6437e9ada04f48
Chen <73538798+Yenchenl@users.noreply.github.com>
2023-09-02 16:45:00 +0800

    add the login page

--- server.js
+++ server.js
@@ -1,26 +1,10 @@
+// import all we need 
 const express = require('express');
 const mysql = require('mysql2');
 const app = express();
 const port = 3000;
 const path = require('path');
 const userRoutes = require('./user'); // 導入 user.js 模組
-
-// 建立與資料庫的連線
-// const db = mysql.createConnection({
-//   host: 'localhost',
-//   user: 'root',
-//   password: 'password',
-//   database: 'member'
-// });
-
-// // 測試資料庫連線
-// db.connect(err => {
-//   if (err) {
-//     console.error('資料庫連線失敗：', err);
-//   } else {
-//     console.log('資料庫連線成功');
-//   }
-// });
 
 // 使用 JSON 解析中間件
 app.use(express.json());
@@ -31,6 +15,27 @@ app.use(express.static('public'));
 // 使用 userRoutes 模組處理用戶相關路由
 app.use('/', userRoutes);
 
+
 app.listen(port, () => {
   console.log(`伺服器運行在 http://localhost:${port}`);
 });
+
+// testing code
+
+// const express = require('express');
+// const path = require('path');
+
+// const app = express();
+// const port = 3000;
+
+// // 使用 express.static 中间件来提供静态文件
+// app.use(express.static('public'));
+
+// // 设置 login.html 为根页面
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'public', 'login.html'));
+// });
+
+// app.listen(port, () => {
+//   console.log(`伺服器運行在 http://localhost:${port}`);
+// });
